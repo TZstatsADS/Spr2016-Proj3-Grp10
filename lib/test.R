09379b941b7d6381eb2c086115b27c24b0063f58
liaoaoyuan97 <al3468@columbia.edu>
2016-03-23 15:53:55 -0400

    Added files via upload

--- lib/test.R
+++ lib/test.R
@@ -1,4 +1,4 @@
 test<-function(fit_train,dat_test){
- l<-list(baseline=predict(fit_train$svm,dat_test[,1:800]),adv=predict(ft_train$rf,dat_test[,801:950]))
- return(l)
- }
+  l<-list(baseline=predict(fit_train$svm,dat_test[,1:800]),adv=predict(fit_train$rf,dat_test[,801:950]))
+  return(l)
+}
--- lib/train.R
+++ lib/train.R
@@ -1,49 +1,27 @@
 
 train <- function(dat_train, label_train) {
   
-        library(e1071)
-        require(caret)
-        require(ggplot2)
-        require(randomForest)
-        library(randomForest)
-        label_train <- as.factor(label_train)
-        
-        # color features + SVM = worst
-#         load("COLOR_FEATURE.RData")
-#         load("y_cat.RData")
-        color_feature<-dat_train[,1:800]
-        best_cost<-25
-        svm_linear<-svm(color_feature,y_cat,type ='C',kernel ='linear',cost=1,gamma =0)
-        
-        
-        # SIFT features + RF = best
-        
-        
-        ##Random Forest(with crossvalidation and SIFT feature)
-
-        rf_model<-train(x=dat_train[,801:950],y=label_train,method='rf',trControl=trainControl(method="cv",number=5))
-        
-        model=list()
-        model$svm<-svm_linear
-        model$rf<-rf_model
-        
-        return(model)
+  library(e1071)
+  require(caret)
+  require(ggplot2)
+  require(randomForest)
+  library(randomForest)
+  label_train <- as.factor(label_train)
+  
+  # color features + SVM = worst
+  color_feature<-dat_train[,1:800]
+  svm_linear<-svm(color_feature,label_train,type ='C',kernel ='linear',cost=1,gamma =0)
+  
+  # SIFT features + RF = best
+  
+  
+  ##Random Forest(with crossvalidation and SIFT feature)
+  sift<-dat_train[,801:950]
+  rf_model<-randomForest(x=sift,y=label_train)
+  
+  model=list()
+  model$svm<-svm_linear
+  model$rf<-rf_model
+  
+  return(model)
 }
-
-
-
-#print(rf_model)
-#print(rf_model$finalModel)
-
-
-
-
-
-
-
-
-#return a list model
-#model$baseline = base model
-#model$adv = best model
-
-# Conclusion: features are more important than models.
